73b4dc9a856a3052d73d8aee2a3c65bdc91eed77
BuildTools <unconfigured@null.spigotmc.org>
2024-02-27 20:37:21 -0700

    Initial commit

---
+++ src/main/java/net/player/bettersnowsports/BetterSnowSportsClient.java
@@ -0,0 +1,10 @@
+package net.player.bettersnowsports;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterSnowSportsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/player/bettersnowsports/item/ModItems.java
@@ -0,0 +1,4 @@
+package net.player.bettersnowsports.item;
+
+public class ModItems   {
+}
